882aeabcf7e07a89cbde96ae15a35c485141dc3a
shekhar-singh07 <shekharchess8@gmail.com>
2026-07-14 20:49:17 +0530

    added bookmark model

--- interactions/models.py
+++ interactions/models.py
@@ -35,3 +35,16 @@ class Follow(models.Model):
             name="unique_follow"
         )
     ]
+class Bookmark(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    bookmarked_at=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(
+            fields=["user", "post"],
+            name="unique_Bookmark"
+        )
+    ]
+    def __str__(self):
+        return f"{self.user.username} bookmarked post {self.post.id}"
